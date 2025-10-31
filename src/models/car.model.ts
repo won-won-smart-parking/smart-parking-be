21d93e38757c4740234249ea94fe5d6a69821fb6
JGW-Korea <jgw6372@gmail.com>
2025-10-31 10:37:25 +0900

    feat: Sequelize Car Model 정의 (#2)

---
+++ src/models/car.model.ts
@@ -0,0 +1,61 @@
+import { DataTypes, Model, type Optional } from "sequelize";
+import { v4 as uuidv4 } from "uuid";
+import User from "./user.model.ts";
+import { sequelize } from "../configs/database.ts";
+
+// 1. 타입 정의
+export interface CarAttributes {
+  id: string;
+  user_id: string;
+  car_number: string;
+}
+
+// 2. 생성 시 id는 자동 생성되므로 Optional
+interface CarCreationAttributes extends Optional<CarAttributes, "id"> {}
+
+// 3. 모델 클래스 정의
+export default class Car extends Model<CarAttributes, CarCreationAttributes> implements CarAttributes {
+  public id!: string;
+  public user_id!: string;
+  public car_number!: string;
+}
+
+// 4. 모델 초기화
+Car.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: () => uuidv4(),
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    car_number: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Car",
+    tableName: "cars",
+    timestamps: true,
+    underscored: true,
+  },
+);
+
+// 5. 관계 설정 (User 1 : N Car)
+User.hasMany(Car, {
+  foreignKey: "user_id",
+  sourceKey: "id",
+  as: "cars",
+});
+
+Car.belongsTo(User, {
+  foreignKey: "user_id",
+  targetKey: "id",
+  as: "user",
+});
