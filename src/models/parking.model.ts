a8a1e4604877520f5ca8deaba9cc7c4538ee4025
JGW-Korea <jgw6372@gmail.com>
2025-10-31 10:37:11 +0900

    feat: Sequelize Parking Model 정의 (#2)

---
+++ src/models/parking.model.ts
@@ -0,0 +1,33 @@
+import { DataTypes, Model, type Optional } from "sequelize";
+import { v4 as uuidv4 } from "uuid";
+import { sequelize } from "../configs/database.ts";
+
+// 타입 정의
+export interface ParkingAttributes {
+  id: string;
+}
+
+interface ParkingCreationAttributes extends Optional<ParkingAttributes, "id"> {}
+
+// 모델 정의
+export default class Parking extends Model<ParkingAttributes, ParkingCreationAttributes> implements ParkingAttributes {
+  public id!: string;
+}
+
+// 초기화
+Parking.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: () => uuidv4(),
+      primaryKey: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Parking",
+    tableName: "parking",
+    timestamps: true,
+    underscored: true,
+  },
+);
