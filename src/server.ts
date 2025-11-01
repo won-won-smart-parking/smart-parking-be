0d587df0232fcf914b2deee3a916824c7aabb48d
JGW-Korea <jgw6372@gmail.com>
2025-11-01 16:40:06 +0900

    update: express 내장 urlencoded 미들웨어 등록 (#4)

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import router from "./router/index.ts";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Content-Type: application/json 요청 본문(Request Body) 자동 해석
+app.use(express.urlencoded({ extended: true })); // Content-Type: application: x-www-form-urlencoded 요청 본문 파싱(qs 모듈을 통해 본문 내의 URL 인코딩 데이터를 해석)
+
 app.use("/", router); // 애플리케이션 서버 라우트 미들웨어 등록
 
 // 웹 서버(Web Server) 오픈
