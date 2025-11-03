8130cb5d627f9e374107204b1b0c523ef6d3e1af
JGW-Korea <jgw6372@gmail.com>
2025-11-03 16:40:49 +0900

    update: 회원가입 API 일반 테이블 생성 -> supabase.auth 기반 수정 (#4)

--- src/router/routes/auth.route.ts
+++ src/router/routes/auth.route.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import type { SignUpRequestBodyType } from "types/auth.type.ts";
+import type { SignInRequestBodyType, SignUpRequestBodyType } from "types/auth.type.ts";
+import { AuthApiError } from "@supabase/supabase-js";
 import { supabase, upload } from "../../configs/index.ts";
-import { encodePassowrd } from "../../utils/bcrypt.ts";
 import { uploadProfileImage } from "../../utils/signUp.ts";
 
 const router = express.Router();
@@ -20,26 +20,27 @@ router.post("/sign-up", upload.single("profileImage"), async (req, res) => {
   try {
     const [newUserInfo, profileImageFile] = [req.body, req.file] as [SignUpRequestBodyType, Express.Multer.File | undefined]; // 클라이언트에서 보낸 body와 file(선택적)을 전달받는다.
 
-    // 입력한 이메일 중복 여부를 검증한다.
-    // - 중복되지 않았을 경우의 결과값: { error: null, data: null, count: 0, status: 200, statusText: 'OK' }
-    // - 중복된 경우의 결과값: { error: null, data: null, count: 1, status: 200, statusText: 'OK' }
-    const { count } = await supabase.from("users").select("*", { count: "exact", head: true }).eq("email", newUserInfo.email);
-    if (count && count > 0) {
-      return res.status(409).json({
-        code: "SIGN_UP_EMAIL_DUPLICATE",
-        message: "입력하신 이메일은 이미 사용 중입니다.",
-        success: false,
-      });
-    }
-
-    const hashedPassword = await encodePassowrd(newUserInfo.password); // 비밀번호를 bcrypt를 통해 사용자가 입력한 비밀번호를 암호화시킨다.
     const profileImageUrl = await uploadProfileImage(profileImageFile); // 사용자 프로필 이미지를 업로드하고 이미지 경로를 반환받는다. (null | string)
+
+    // Supabase Auth에 접근하여 사용자 정보를 구성한다.
+    const { data, error: authSingUpError } = await supabase.auth.signUp({
+      email: newUserInfo.email,
+      password: newUserInfo.password,
+    });
+
+    // 에러가 발생한 경우
+    if (authSingUpError) {
+      throw authSingUpError;
+    }
 
     // 위 절차가 모두 에러 없이 통과한 경우 Supabase DB에 유저 정보를 추가한다.
     const { error } = await supabase.from("users").insert({
-      ...newUserInfo,
-      password: hashedPassword,
+      id: data.user?.id,
+      name: newUserInfo.name,
+      birth: newUserInfo.birth,
       profileImageUrl,
+      isLocationAgreed: newUserInfo.isLocationAgreed,
+      isAlarmAgreed: newUserInfo.isAlarmAgreed,
     });
 
     // Supabase DB에 접근하여 오류가 발생한 경우 예외를 발생시킨다.
@@ -47,15 +48,31 @@ router.post("/sign-up", upload.single("profileImage"), async (req, res) => {
       throw error;
     }
 
-    // 성공 응답 전송
-    return res.status(200).json({
+    return res.status(201).json({
       code: "SIGN_UP_SUCCESS",
       message: "회원가입이 정상적으로 완료되었습니다.",
       success: true,
+      payload: {
+        id: data.user?.id,
+        email: data.user?.email,
+        profilImageUrl: profileImageUrl,
+        accessToken: data.session?.access_token,
+        refreshToken: data.session?.refresh_token,
+      },
     });
   } catch (error) {
+    if (error instanceof AuthApiError) {
+      return res.status(error.status).json({
+        code: error.code,
+        errorType: "AuthApiError",
+        message: error.message,
+        success: false,
+      });
+    }
+
     return res.status(500).json({
       code: "SIGN_UP_ERROR",
+      type: "Server",
       success: false,
       error,
     });
@@ -72,6 +89,47 @@ router.post("/sign-up", upload.single("profileImage"), async (req, res) => {
   - 비밀번호 일치 여부 확인 -> 실패 시 401 Unauthorized + SIGN_IN_FAILED 반환
   - 위 조건 모두 통과 시 -> 200 OK + SIGN_IN_SUCCESS 반환
 */
+router.post("/sign-in", async (req, res) => {
+  try {
+    const { email, password } = req.body as SignInRequestBodyType;
+
+    // Supabase Auth를 통한 로그인 시도
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    // 사용자 정보가 없거나, 비밀번호가 일치하지 않아 로그인 실패 시 실패 응답을 반환한다.
+    if (error) {
+      console.log(error);
+      return res.status(401).json({
+        code: "SIGN_IN_FAILED",
+        message: "아이디 또는 비밀번호가 일치하지 않습니다.",
+        success: false,
+      });
+    }
+
+    // 로그인 성공 시 -> Supabase에서 발급한 Access Token + Refresh Token을 통해 응답 결과를 반환한다.
+    return res.status(200).json({
+      code: "SIGN_IN_SUCCESS",
+      message: "로그인에 성공했습니다.",
+      success: true,
+      body: {
+        id: data.user.id,
+        email: data.user.email,
+        accessToken: data.session.access_token,
+        refreshToken: data.session.refresh_token,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: "SIGN_IN_ERROR",
+      success: false,
+      error,
+    });
+  }
+});
+
 // router.post("/sign-in", async (req, res) => {
 //   const { email, password } = req.body as { email: string; password: string };
 
@@ -83,17 +141,8 @@ router.post("/sign-up", upload.single("profileImage"), async (req, res) => {
 //     attributes: ["id", "email", "password"],
 //   });
 
-//   // 사용자 정보가 존재하지 않는 경우 -> 실패 응답을 전송한다.
-//   if (!user) {
-//     return res.status(401).json({
-//       code: "SIGN_IN_FAILED",
-//       message: "아이디 또는 비밀번호가 일치하지 않습니다.",
-//       success: false,
-//     });
-//   }
-
 //   // 사용자 정보가 존재하는 경우 -> 비밀번호 일치 여부를 확인한다.
-//   // const isPasswordMatch = await comparePassword(password, user.password); -> 회원가입 이후 bcrypt 정확히 수행 후 리팩토링
+// const isPasswordMatch = await comparePassword(password, user.password); -> 회원가입 이후 bcrypt 정확히 수행 후 리팩토링
 //   if (password !== user.password) {
 //     return res.status(401).json({
 //       code: "SIGN_IN_FAILED",
