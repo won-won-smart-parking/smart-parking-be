68c35af5c80031d3d0505eefe57986245ec0e018
JGW-Korea <jgw6372@gmail.com>
2025-11-03 13:10:45 +0900

    feat: 사용자 비밀번호 bcrypt 암호화 유틸 함수 로직 구성 (#4)

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,5 +1,10 @@
 import bcrypt from "bcrypt";
 
+// 사용자 비밀번호 bcrypt 암호화
+export async function encodePassowrd(plain: string) {
+  return await bcrypt.hash(plain, await bcrypt.genSalt(10));
+}
+
 // 암호화 값 일치 여부
 export async function comparePassword(plain: string, hashed: string): Promise<boolean> {
   return await bcrypt.compare(plain, hashed);
