a20c1e20a59c711b1261275e634c58760d0064b5
JGW-Korea <jgw6372@gmail.com>
2025-11-03 16:39:55 +0900

    update: 이미지 파일명 잘못된 구성 수정 (#4)

--- src/utils/signUp.ts
+++ src/utils/signUp.ts
@@ -15,12 +15,13 @@ export async function uploadProfileImage(file: Express.Multer.File | undefined) 
   }
 
   // 업로드 이미지 파일 이름을 구성하고, webp 확장자로 파일을 변경한다.
-  const uploadFileName = `${(path.basename(file.originalname), path.extname(file.originalname))}-${Date.now()}.webp`;
+  const uploadFileName = `${path.basename(file.originalname, path.extname(file.originalname))}-${Date.now()}.webp`;
   const webpBuffer = await sharp(file.buffer).webp().toBuffer();
 
   // Supabase Storage에 이미지 업로드
   const { data, error } = await supabase.storage.from("profile-images").upload(`users/${uploadFileName}`, webpBuffer, {
     contentType: "image/webp",
+    upsert: false,
   });
 
   // 이미지 업로드 과정에서 에러가 발생한 경우
